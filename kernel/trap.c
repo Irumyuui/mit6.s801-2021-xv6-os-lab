38ecd1f6fe13c3a6583702b37b5e06446c9a5322
ChirunoC9 <ciel_katana@outlook.com>
2024-02-02 21:09:26 +0800

    submit 'Eliminate allocation from sbrk()' solution

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,21 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if (r_scause() == 13 || r_scause() == 15) {
+    // 13 load page fault
+    // 15 store/AMO page fault
+
+    uint64 va = PGROUNDDOWN(r_stval());
+    uint64 pa = (uint64)kalloc();
+    if (pa == 0) {
+      p->killed = 1;
+    } else {
+      memset((void *)pa, 0, PGSIZE);
+      if (mappages(p->pagetable, va, PGSIZE, pa, PTE_W | PTE_R | PTE_U) != 0) {
+        kfree((void *)pa);
+        p->killed = 1;
+      }
+    }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
